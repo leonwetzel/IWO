66492048a3482907daaa001863cf304b2d89fab4
Leon Wetzel <leonwetzel@gmail.com>
2020-03-28 21:40:45 +0100

    + Added gitgnore and first version of application

---
+++ main.py
@@ -0,0 +1,52 @@
+import io
+
+import pandas as pd
+import requests
+from pandas import ExcelWriter
+
+
+def main():
+    """
+    Retrieve relevant statistics from the police statements
+    file of the Municipality of Groningen.
+
+    Since the data sheet provided by the Municipality cannot be directly used
+    by R, we use this application to rearrange the data in a proper way.
+    :return:
+    """
+    url = "https://ckan.dataplatform.nl/dataset/7498165c-c40d-459e-9f75-df5eb28c30d3/resource/341f5da0-a802-4975-8ef5-84cefd9c6dfb/download/aangifte-politie_buurten.csv"
+    s = requests.get(url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), delimiter=';',
+                     header=[0, 1], index_col=0)
+    df.columns = create_column_names(df)
+    df.index.name = "buurtnummer"
+    df = df[:30]
+    df = df.drop([140109, 140108])  # excludes the Martini Trade Park and Stadspark areas
+
+    with ExcelWriter('data.xlsx', engine='xlsxwriter') as writer:
+        df.to_excel(writer)
+
+
+def create_column_names(df):
+    """
+    Creates new column names, based on the first two rows in the data sheet.
+    :param df:
+    :return:
+    """
+    current_column = None
+    headers = []
+    for first_name, second_name in list(df):
+        if not current_column:
+            current_column = first_name
+        elif "Unnamed" not in first_name:
+            current_column = first_name
+
+        if second_name == 'x':
+            headers.append(current_column)
+        else:
+            headers.append("{} {}".format(current_column, second_name))
+    return headers
+
+
+if __name__ == '__main__':
+    main()
